676c914d71b04cdd7451d9ba589262e8ef5dbd0d
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-11 00:41:40 +0200

    UpdateTelementryController

--- NWU-Tech-Trends/Controllers/TelementryController.cs
+++ NWU-Tech-Trends/Controllers/TelementryController.cs
@@ -1,6 +1,20 @@
-﻿namespace NWU_Tech_Trends.Controllers
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NWU_Tech_Trends.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NWU_Tech_Trends.Controllers
 {
     public class TelementryController
     {
+
     }
 }
