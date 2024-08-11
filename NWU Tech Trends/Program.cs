9d9594ed4faf9f89178e50e86e35f7ca47d6c5ee
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-11 03:49:35 +0200

    Update NuGet Packages

--- NWU Tech Trends/Program.cs
+++ NWU Tech Trends/Program.cs
@@ -1,5 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using NWU_Tech_Trends.Models;
+using NWU_Tech_Trends.Authentication;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Linq;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
