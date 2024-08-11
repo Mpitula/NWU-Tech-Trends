76f9e88314f8a27712de30a99f080c8c2fbab4ee
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-11 03:23:16 +0200

    Authorise clientcontroller

--- NWU Tech Trends/Controllers/WeatherForecastController.cs
+++ NWU Tech Trends/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NWU_Tech_Trends.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
