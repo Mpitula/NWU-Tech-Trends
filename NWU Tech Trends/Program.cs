e08519fa153d7a3927b4f7787160443fc629f457
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-11 11:20:50 +0200

    Update program.cs

--- NWU Tech Trends/Program.cs
+++ NWU Tech Trends/Program.cs
@@ -1,21 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using NWU_Tech_Trends.Models;
-//using NWU_Tech_Trends.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.OpenApi;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using Swashbuckle.AspNetCore.SwaggerUI;
-using System.Linq;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,40 +10,6 @@ builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<Alonempitula37460366Context>(options => options.UseSqlServer(connectionString));
-
-// For Identity  
-builder.Services.AddIdentity<Client, IdentityRole>()
-    .AddEntityFrameworkStores<DbContext>()
-    .AddDefaultTokenProviders();
-
-// Adding Authentication  
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-            // Adding Jwt Bearer  
-            .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidIssuer = builder.Configuration["Jwt:ValidIssuer"],
-                    ValidAudience = builder.Configuration["Jwt:ValidAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]))
-                };
-            });
-
-builder.Services.AddAuthorization();
-
-// Add configuration from appsettings.json
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
