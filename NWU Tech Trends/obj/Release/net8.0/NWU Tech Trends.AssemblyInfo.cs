20373dbd51f0a83414737012d64a81eb07ac3d76
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-11 09:49:21 +0200

    Publish Failed

--- NWU Tech Trends/obj/Release/net8.0/NWU Tech Trends.AssemblyInfo.cs
+++ NWU Tech Trends/obj/Release/net8.0/NWU Tech Trends.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWU Tech Trends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35faab023c1717ed1ebb326d693acbecc8688a61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+194a080a262c62fa5335487d55d8a91a8b0bb5e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NWU Tech Trends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWU Tech Trends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
