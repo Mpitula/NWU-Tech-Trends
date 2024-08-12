42041423eba79819a29723e54e766982de9e9f01
A Mpitula <37460366@mynwu.ac.za>
2024-08-12 14:14:09 +0200

    Publish

--- NWU Tech Trends/obj/Release/net8.0/NWU Tech Trends.AssemblyInfo.cs
+++ NWU Tech Trends/obj/Release/net8.0/NWU Tech Trends.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWU Tech Trends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbd55d67b44667a9cffc6924a05db1b68f373d8")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c6e5873a63a1be107173c9862294f20060f363")]
+>>>>>>> Publish
 [assembly: System.Reflection.AssemblyProductAttribute("NWU Tech Trends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWU Tech Trends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
